be2fb57da1714410409f80279b6a40337ea3b83f
David Kim <rtdk0324@gmail.com>
2018-12-16 22:32:39 -0500

    final commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,49 @@
 $(document).ready(function() {
+  // toggler navbar btn
+  $(".navbar-toggler").click(function() {
+    $(".navbar-toggler").toggleClass("change");
+  });
+
+  const navbarTop = document.querySelector("#navbar-id").offsetTop;
+
+  // sticky navbar less paddin
+  $(window).scroll(function() {
+    console.log(navbarTop);
+    let position = $(this).scrollTop();
+    if (position >= navbarTop) {
+      $(".navbar").addClass("navbar-background");
+      $(".navbar").addClass("fixed-top");
+    } else {
+      $(".navbar").removeClass("navbar-background");
+      $(".navbar").removeClass("fixed-top");
+    }
+  });
+
+  // smooth scroll
+  $(".nav-item a, .header-link, #back-to-top").click(function(link) {
+    link.preventDefault();
+
+    let target = $(this).attr("href");
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(target).offset().top - 20
+        },
+        3000
+      );
+  });
+
+  // back to top
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if (position >= navbarTop) {
+      $("#back-to-top").addClass("scrollTop");
+    } else {
+      $("#back-to-top").removeClass("scrollTop");
+    }
+  });
+
   // ripples
   $("#header, .info").ripples({
     dropRadius: 25,
